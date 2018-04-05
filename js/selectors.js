6bf0b4b81887c5374f085923a5f1bf5378f17a0a
Chase <chaseit12@gmail.com>
2018-04-05 21:29:11 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,25 @@
 // declare your functions here...
 
 
+$(function(){
+  
+  function paragraphSelector(){
+    return $('p')
+  }
+  
+  function lastImageSelector(){
+    return $('img:last')
+  }
+  
+  function ninjaBabySelector(){
+    return $('#baby-ninja')
+  }
+  
+  function divSelect(){
+    return $('.pics')
+  }
+  
+  function firstListItem(){
+    return $('ul li:#pic-list')
+  }
+})
